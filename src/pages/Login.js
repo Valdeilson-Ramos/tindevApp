72eb09c4468c3a5ec67a911a3b6bd030db77183e
valdeilson <vautronica@gmail.com>
2019-09-11 20:35:17 -0300

    configurando acesso a API

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import {
   Text,
   View,
@@ -8,13 +9,29 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
+import api from '../services/api';
+
 import logo from '../assets/logo.png';
 
 export default function Login({navigation}) {
   const [user, SetUser] = useState('');
 
-  function handleLogin() {
-    navigation.navigate('Main');
+  useEffect(()=> {
+    AsyncStorage.getItem('user').then(user =>{
+      if (user) {
+        navigation.navigate('Main', {user});
+      }
+    })
+  }, []);
+
+  async function handleLogin() {
+    const response = await api.post('/devs', {username: user});
+
+    const {_id} = response.data;
+
+    await AsyncStorage.setItem('user', _id);
+
+    navigation.navigate('Main', {_id});
   }
 
   return (
